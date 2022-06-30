6b664306c3aab111352e45704abdce8b25bbcef1
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-06-30 22:59:34 +0700

    Create container.tsx

---
+++ src/components/layout/container.tsx
@@ -0,0 +1,21 @@
+import clsx from "~/src/utils/clsx";
+import { ContainerProps } from "~types/components";
+
+export default function Container(props: ContainerProps) {
+  const { size, className, children } = props;
+
+  function containerSize() {
+    switch (size) {
+      case "md":
+        return "max-w-[840px] mid:m-auto";
+      case "lg":
+        return "max-w-[1240px] lg:m-auto";
+      case "xl":
+        return "max-w-[1440px] xl:m-auto";
+      default:
+        return "max-w-[1440px] xl:m-auto";
+    }
+  }
+
+  return <div className={clsx(containerSize(), className, "mx-5")}>{children}</div>;
+}
