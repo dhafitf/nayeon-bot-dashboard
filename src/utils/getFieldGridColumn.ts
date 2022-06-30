77ee63f447c78f5b08a27578b50ab98364d31f33
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-06-30 23:07:24 +0700

    Create getFieldGridColumn.ts

---
+++ src/utils/getFieldGridColumn.ts
@@ -0,0 +1,31 @@
+import { DiscordEmbedFieldProps } from "~types/discord";
+
+const MAX_FIELDS_PER_ROW = 3;
+const FIELD_GRID_SIZE = 12;
+
+export const getFieldGridColumn = (fields: any, field: DiscordEmbedFieldProps, index: number) => {
+  if (!field.inline) return `1 / ${FIELD_GRID_SIZE + 1}`;
+
+  let startingField = index;
+  while (startingField > 0 && fields[startingField - 1].inline) {
+    startingField -= 1;
+  }
+
+  let totalInlineFields = 0;
+  while (fields.length > startingField + totalInlineFields && fields[startingField + totalInlineFields].inline) {
+    totalInlineFields += 1;
+  }
+
+  const indexInSequence = index - startingField;
+  const currentRow = indexInSequence / MAX_FIELDS_PER_ROW;
+  const indexOnRow = indexInSequence % MAX_FIELDS_PER_ROW;
+  const totalOnLastRow = totalInlineFields % MAX_FIELDS_PER_ROW || MAX_FIELDS_PER_ROW;
+  const fullRows = (totalInlineFields - totalOnLastRow) / MAX_FIELDS_PER_ROW;
+  const totalOnRow = currentRow >= fullRows ? totalOnLastRow : MAX_FIELDS_PER_ROW;
+
+  const columnSpan = FIELD_GRID_SIZE / totalOnRow;
+  const start = indexOnRow * columnSpan + 1;
+  const end = start + columnSpan;
+
+  return `${start} / ${end}`;
+};
