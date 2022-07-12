91bd6295a532ae32df0356b754964db7b677878c
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-07-12 16:40:23 +0700

    Update embedFooter.tsx

--- src/components/section/discord/embeds/embedFooter.tsx
+++ src/components/section/discord/embeds/embedFooter.tsx
@@ -7,7 +7,9 @@ export default function DiscordEmbedFooter(props: DiscordEmbedFooterProps) {
 
   return (
     <div className="discord-embed-footer">
-      {icon_url && <div className="discord-embed-footer-icon">{icon_url && <Image src={icon_url} alt={`${text}`} width={20} height={20} className="rounded-full" />}</div>}
+      {icon_url && (
+        <div className="discord-embed-footer-icon">{icon_url && <Image src={icon_url} alt={`${text}`} objectFit="cover" width={20} height={20} className="rounded-full" />}</div>
+      )}
       <span className="text-[10px]">
         {text && <span className="discord-embed-footer-text">{text}</span>}
         {text && timestamp && <span className="discord-embed-footer-separator mx-1">•</span>}
