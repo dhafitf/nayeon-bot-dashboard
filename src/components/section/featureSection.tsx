2fbaf676734c8024b62432ff1764647cc310109b
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-07-12 16:47:50 +0700

    create featureSection

---
+++ src/components/section/featureSection.tsx
@@ -0,0 +1,79 @@
+import DiscordMessage from "@section/discord/discordMessage";
+import Links from "@other/links";
+import { MdSpaceDashboard, MdNotificationsActive } from "react-icons/md";
+import { FaChartBar } from "react-icons/fa";
+import { socmedFeatureMessage, transtaleFeatureMessage } from "~utils/lib/featureMessageData";
+
+const upcomingFeatures = [
+  {
+    icon: <MdNotificationsActive />,
+    title: "Role Mention",
+    description: "Want to get notified when there's a new update? This feature will notify you when there is an update.",
+  },
+  {
+    icon: <MdSpaceDashboard />,
+    title: "Dashboard",
+    description: "Simplify server settings with dashboard.",
+  },
+  {
+    icon: <FaChartBar />,
+    title: "Leveling System",
+    description: "Leveling system to make servers more active and increase user engagement.",
+  },
+];
+
+export default function FeatureSection() {
+  return (
+    <div id="features" className="pb-8">
+      <div className="flex gap-8 md:flex-row-reverse max-md:flex-col">
+        <div className="flex-1 md:flex md:flex-col md:justify-center">
+          <h2 className="text-3xl font-bold max-md:text-center">Socmed Update</h2>
+          <p className="pt-6 max-md:text-center">Nayeon bot will notify available updates from TWICE, now includes updates from: </p>
+          <ul className="list-disc py-4 px-6 pl-4">
+            <li>Instagram</li>
+            <li>VLive</li>
+            <li>Youtube</li>
+            <li>Tiktok</li>
+          </ul>
+          <div className="max-md:text-center">
+            <Links className="text-light-blue hover:underline" text="Read More" href="/docs/socmed" />
+          </div>
+        </div>
+        <div className="scrollbar max-h-[500px] flex-1 overflow-auto">
+          <DiscordMessage message={socmedFeatureMessage} />
+        </div>
+      </div>
+      <div className="my-10 mx-5 h-[2px] max-w-[1440px] rounded bg-[#4E4F50] xl:mx-auto"></div>
+      <div className="flex gap-8 md:flex-row max-md:flex-col">
+        <div className="flex-1 md:flex md:flex-col md:justify-center">
+          <h2 className="text-3xl font-bold max-md:text-center">Translator</h2>
+          <p className="pt-6 pb-4 max-md:text-center">
+            Nayeon bot can translate the sentence or word you ask. You can set the text to be translated to a language with{" "}
+            <code className="whitespace-nowrap rounded bg-[#4c4f54] px-1">?[lang]</code>. <span className="text-sm italic">(The default language is English)</span>
+          </p>
+          <div className="max-md:text-center">
+            <Links className="text-light-blue hover:underline" text="Read More" href="/docs/translator" />
+          </div>
+        </div>
+        <div className="scrollbar max-h-[500px] flex-1 overflow-auto">
+          <DiscordMessage message={transtaleFeatureMessage} />
+        </div>
+      </div>
+      <div className="my-10 mx-5 h-[2px] max-w-[1440px] rounded bg-[#4E4F50] xl:mx-auto"></div>
+      <div className="flex flex-col items-center">
+        <h2 className="pb-6 text-center text-3xl font-bold">Upcoming update</h2>
+        <div className="grid gap-4 md:grid-cols-3">
+          {upcomingFeatures.map(({ icon, title, description }, index) => {
+            return (
+              <div key={index} className="flex max-w-md flex-col items-center gap-2 rounded-md bg-[#3c4146] p-6">
+                <span className="text-6xl">{icon}</span>
+                <span className="text-lg font-bold">{title}</span>
+                <div className="text-center text-sm">{description}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/utils/lib/featureMessageData.js
@@ -0,0 +1,78 @@
+export const socmedFeatureMessage = [
+  {
+    name: "Nayeon",
+    icon_url: "https://cdn.discordapp.com/attachments/946950887480369154/991575129572257862/25cdd9a7f2eb3f8cee43fefcfc0d6100f09c704d.jpg",
+    isBot: true,
+    color: "#4791ff",
+    embeds: [
+      {
+        color: "#EA00FF",
+        author: {
+          name: "TWICE Instagram update",
+          icon_url: "https://cdn.discordapp.com/attachments/946644664134533161/992087715711565844/channels4_profile.jpg",
+        },
+        description: "힐링 \n \\n\\nLink:\\n https://www.instagram.com/p/CDomKI1jvYu/",
+        timestamp: "August 8, 2020",
+      },
+    ],
+    attachment: [
+      "https://cdn.discordapp.com/attachments/946644664134533161/992091578225152020/117534505_113703233626902_3281974805927365228_n.jpg",
+      "https://cdn.discordapp.com/attachments/946644664134533161/992092679515160647/117309397_1684684131682914_7691563183807373715_n.jpg",
+    ],
+  },
+];
+
+export const transtaleFeatureMessage = [
+  {
+    name: "devzfz",
+    icon_url: "https://cdn.discordapp.com/attachments/946950887480369154/991575129572257862/25cdd9a7f2eb3f8cee43fefcfc0d6100f09c704d.jpg",
+    isBot: false,
+    color: "#ffad47",
+    timestamp: "Today at 10:10 PM",
+    content: ";t おはようございます",
+  },
+  {
+    name: "Nayeon",
+    icon_url: "https://cdn.discordapp.com/attachments/946950887480369154/991575129572257862/25cdd9a7f2eb3f8cee43fefcfc0d6100f09c704d.jpg",
+    isBot: true,
+    color: "#4791ff",
+    timestamp: "Today at 10:10 PM",
+    embeds: [
+      {
+        color: "#4752c4",
+        author: {
+          name: "devzfz#1415",
+          icon_url: "https://cdn.discordapp.com/attachments/946644664134533161/992087715711565844/channels4_profile.jpg",
+        },
+        description: "good morning",
+        timestamp: "Japanese → English",
+      },
+    ],
+  },
+  {
+    name: "devzfz",
+    icon_url: "https://cdn.discordapp.com/attachments/946950887480369154/991575129572257862/25cdd9a7f2eb3f8cee43fefcfc0d6100f09c704d.jpg",
+    isBot: false,
+    color: "#ffad47",
+    timestamp: "Today at 10:11 PM",
+    content: ";t ?id おはようございます",
+  },
+  {
+    name: "Nayeon",
+    icon_url: "https://cdn.discordapp.com/attachments/946950887480369154/991575129572257862/25cdd9a7f2eb3f8cee43fefcfc0d6100f09c704d.jpg",
+    isBot: true,
+    color: "#4791ff",
+    timestamp: "Today at 10:11 PM",
+    embeds: [
+      {
+        color: "#4752c4",
+        author: {
+          name: "devzfz#1415",
+          icon_url: "https://cdn.discordapp.com/attachments/946644664134533161/992087715711565844/channels4_profile.jpg",
+        },
+        description: "Selamat pagi",
+        timestamp: "Japanese → Indonesian",
+      },
+    ],
+  },
+];
