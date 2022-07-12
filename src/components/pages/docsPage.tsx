f01ac3a71bb55012dade3d075a9bbac37780d38d
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-07-12 17:19:02 +0700

    create docs page

---
+++ src/components/pages/docsPage.tsx
@@ -0,0 +1,28 @@
+import GeneralPage from "@layout/general";
+import { NextSeo } from "next-seo";
+import DocsSidebar from "@section/navigation/docsSidebar";
+import Container from "@layout/container";
+import { DocsLayoutProps } from "~types/components";
+
+export default function DocsLayout({ children, title, description }: DocsLayoutProps) {
+  const metaTitle = title ? `${title}` : "Nayeon's Docs";
+  const metaDescription = description ? `${description}` : "Documentation bot for Nayeon bot";
+  return (
+    <>
+      <NextSeo
+        title={metaTitle}
+        description={metaDescription}
+        openGraph={{
+          title: "Nayeon's Docs",
+          description: "A list of documentation for Nayeon bot",
+        }}
+      />
+      <GeneralPage>
+        <Container size="md" className="mb-8 md:flex">
+          <DocsSidebar />
+          <article className="prose prose-invert w-full py-4 md:my-8 md:ml-16">{children}</article>
+        </Container>
+      </GeneralPage>
+    </>
+  );
+}
