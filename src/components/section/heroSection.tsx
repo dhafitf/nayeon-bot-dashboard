3f16d6cbfc7ba7050e97f2af0a0209f849c8a5ef
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-09-12 15:42:25 +0700

    change image profile

--- src/components/section/heroSection.tsx
+++ src/components/section/heroSection.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
       <div className="relative mx-auto mt-11 mb-8 aspect-square w-full max-w-[314px]">
         <Image
           className="rounded-full"
-          src="https://cdn.discordapp.com/avatars/939369011773321336/677d9e150f1521716a2188e14ceeca94.png?size=2048"
+          src="https://cdn.discordapp.com/avatars/939369011773321336/c1aa2d7a6d6ed8ac596e8b5bc30a8314.png?size=2048"
           layout="fill"
           alt="nayeon-bot"
           objectFit="cover"
@@ -17,7 +17,7 @@ export default function HeroSection() {
       </div>
       <div className="md:ml-8 mid:m-0 mid:max-w-md">
         <h1 className="text-4xl font-bold max-md:text-center">Nayeon Bot</h1>
-        <p className="pt-6 pb-8 text-lg max-md:text-center">Nayeon bot is a bot dedicated to notifying updates from TWICE, and also provides other commands.</p>
+        <p className="pt-6 pb-8 text-lg max-md:text-center">Nayeon bot is a discord bot dedicated to notifying updates from TWICE, and also provides other commands.</p>
         <div className="flex items-center gap-6 max-md:flex-col max-md:justify-center">
           <InviteButton className="w-48 justify-center gap-3 py-2 text-lg" />
           <Link href="/server">
