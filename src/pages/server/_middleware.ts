0895e2f84652c507e3e433f147e0fd2b023aa3db
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-09-15 10:13:15 +0700

    Update _middleware.ts

--- src/pages/server/_middleware.ts
+++ src/pages/server/_middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { API_URL } from "~utils/constants";
 
 const validateCookies = (req: NextRequest) => {
   const sessionID = req.cookies["nayeon.sid"];
@@ -7,17 +8,21 @@ const validateCookies = (req: NextRequest) => {
 };
 
 export async function middleware(req: NextRequest) {
-  const headers = validateCookies(req);
-  if (!headers) return NextResponse.redirect(new URL("/login", req.url));
+  try {
+    const headers = validateCookies(req);
+    if (!headers) return NextResponse.redirect(new URL("/login", req.url));
 
-  if (req.page.name === "/server/[...id]") {
-    if (!req.page.params) return NextResponse.redirect(new URL("/", req.url));
-    const response = await fetch(`${process.env.API_URL}/auth/status`, {
-      headers,
-    });
+    if (req.page.name === "/server/[...id]") {
+      if (!req.page.params) return NextResponse.redirect(new URL("/", req.url));
+      const response = await fetch(`${API_URL}/auth/status`, {
+        headers,
+      });
 
-    return response.status === 200 ? NextResponse.next() : NextResponse.redirect(new URL("/login", req.url));
+      return response.status === 200 ? NextResponse.next() : NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.log(error);
   }
-
-  return NextResponse.next();
 }
